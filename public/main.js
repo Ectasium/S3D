7c52ed9d156af66bd711a3bc1b57422f1035046c
Ectasium <dietrich.kluge@gmail.com>
2022-09-24 15:17:47 +0200

    1st quiz prototype added to basketball modal

--- public/main.js
+++ public/main.js
@@ -189,7 +189,7 @@ function init () {
     printer = add_printer();
     door = add_door();
     
-    // BUTTONS/////////////////////////////////////////////////////////////
+    // BUTTONS /////////////////////////////////////////////////////////////
 
     function buttons_init() {
         //Initialize and hide buttons
@@ -294,8 +294,7 @@ function init () {
             let hl4text = document.createTextNode("How Secure is Your Desktop Computer?");
             hl4.appendChild(hl4text);
             let main = document.querySelector('.flex-container > .main-content');
-            main.insertAdjacentElement("afterbegin", hl4);
-                      
+            main.insertAdjacentElement("afterbegin", hl4);                      
 
             // Change description //////////////////////////
             
@@ -328,8 +327,7 @@ function init () {
             let main = document.querySelector('.flex-container > .main-content');
             main.insertAdjacentElement("afterbegin", hlstart);
             controls.enablePan = false;
-            controls.enabled = false;
-            
+            controls.enabled = false;            
 
             // Change description //////////////////////////
             
@@ -446,6 +444,109 @@ function onPointerMove(event) {
 
 };
 
+// Generate Quizzes ////////////////////////////////////////////////////////////////////
+
+function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
+
+	function showQuestions(questions, quizContainer){
+        // we'll need a place to store the output and the answer choices
+        var output = [];
+        var answers;
+    
+        // for each question...
+        for(var i=0; i<questions.length; i++){
+            
+            // first reset the list of answers
+            answers = [];
+    
+            // for each available answer to this question...
+            for(letter in questions[i].answers){
+    
+                // ...add an html radio button
+                answers.push(
+                    '<label>'
+                        + '<input type="radio" name="question'+i+'" value="'+letter+'">'
+                        + letter + ': '
+                        + questions[i].answers[letter]
+                    + '</label>'
+                );
+            }
+    
+            // add this question and its answers to the output
+            output.push(
+                '<div class="question">' + questions[i].question + '</div>'
+                + '<div class="answers">' + answers.join('') + '</div>'
+            );
+        }
+    
+        // finally combine our output list into one string of html and put it on the page
+        quizContainer.innerHTML = output.join('');
+    };
+
+	function showResults(questions, quizContainer, resultsContainer){
+	
+        // gather answer containers from our quiz
+        var answerContainers = quizContainer.querySelectorAll('.answers');
+        
+        // keep track of user's answers
+        var userAnswer = '';
+        var numCorrect = 0;
+        
+        // for each question...
+        for(var i=0; i<questions.length; i++){
+    
+            // find selected answer
+            userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
+            
+            // if answer is correct
+            if(userAnswer===questions[i].correctAnswer){
+                // add to the number of correct answers
+                numCorrect++;
+                
+                // color the answers green
+                answerContainers[i].style.color = 'lightgreen';
+            }
+            // if answer is wrong or blank
+            else{
+                // color the answers red
+                answerContainers[i].style.color = 'red';
+            }
+        }
+    
+        // show number of correct answers out of total
+        resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
+    };
+
+	// show the questions
+	showQuestions(questions, quizContainer);
+
+	// when user clicks submit, show results
+	submitButton.onclick = function(){
+		showResults(questions, quizContainer, resultsContainer);
+	};
+};
+
+var quizContainer = document.getElementById('quiz');
+var resultsContainer = document.getElementById('results');
+var submitButton = document.getElementById('submit');
+
+var myQuestions = [
+	{
+		question: "How can an USB drive be misused?",
+		answers: {
+			a: 'Storing data',
+			b: 'Spreading malware',
+			c: 'No misuse possible'
+		},
+		correctAnswer: 'b'
+	},
+];
+
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+
+// Generate Quizzes End /////////////////////////////////////////////////////////////////
+
+
 window.addEventListener('mousemove', onPointerMove );
 
 // rendering scene and camera
@@ -456,8 +557,8 @@ const render = () => {
 // animation recursive function
 function animate () {
     requestAnimationFrame(animate);
-    world.rotation.y += 0.005;
     render();
+    world.rotation.y += 0.005;
 };
 
  // making canvas responsive
