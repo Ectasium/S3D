5be23f5a1ac434e8a184d8f0b06653692c915d58
Ectasium <dietrich.kluge@gmail.com>
2022-10-05 16:18:26 +0200

    minor corrections

--- public/main.js
+++ public/main.js
@@ -209,10 +209,8 @@ function init () {
          score = gltf.scene.children[0];
          score.visible = true;
          score.scale.set(10, 10, 10);
-         score.position.set(0, 0.7, 0);
-         
+         score.position.set(0, 0.7, 0);         
      });
-
 
     bin = add_bin();
     printer = add_printer();
@@ -337,8 +335,7 @@ function init () {
             let description4 = document.createElement('p');
             let description4text = document.createTextNode("Workplace: Zwei flinke Boxer jagen die quirlige Eva und ihren Mops durch Sylt. Franz jagt im komplett verwahrlosten Taxi quer durch Bayern. Zwölf Boxkämpfer jagen Viktor quer über den großen Sylter Deich. Vogel Quax zwickt Johnys Pferd Bim. Sylvia wagt quick den Jux bei Pforzheim. Polyfon zwitschernd aßen Mäxchens Vögel Rüben, Joghurt und Quark. Fix, Schwyz! quäkt Jürgen blöd vom Paß. Victor jagt zwölf Boxkämpfer quer über den großen Sylter Deich. Falsches Üben von Xylophonmusik quält jeden größeren Zwerg.");
             description4.appendChild(description4text);
-            description3.replaceWith(description4);
-                                    
+            description3.replaceWith(description4);                                    
         });
 
     // Button show Score page
@@ -369,8 +366,7 @@ function init () {
         let description5text = document.createTextNode("The End of the Training: Thank you very much. We hope you had the same fun playing what we had creating this game. See you next time!");
         description5.appendChild(description5text);
         description4.replaceWith(description5);
-        alert("You Correctly Answered " + window.numCorrect + " Questions.");  
-                             
+        alert("You Correctly Answered " + window.numCorrect + " Questions.");                             
     });
         
         //Button restart
@@ -404,8 +400,7 @@ function init () {
             let descriptionstart = document.createElement('p');
             let descriptionstarttext = document.createTextNode("Global Work Environment: Auch gibt es niemanden, der den Schmerz an sich liebt, sucht oder wünscht, nur, weil er Schmerz ist, es sei denn, es kommt zu zufälligen Umständen, in denen Mühen und Schmerz ihm große Freude bereiten können. Um ein triviales Beispiel zu nehmen, wer von uns unterzieht sich je anstrengender körperlicher Betätigung, außer um Vorteile daraus zu ziehen? ");
             descriptionstart.appendChild(descriptionstarttext);            
-            description4.replaceWith(descriptionstart);   
-                      
+            description4.replaceWith(descriptionstart);                         
         });
     };
     buttons_init();
@@ -505,7 +500,7 @@ var moveOnObjects = function (event) {
                 break;
 
             case 'note_cube':
-                    note.visible = true;
+                note.visible = true;
                     break;
 
             case 'bin_cube':
@@ -603,7 +598,7 @@ function generateQuiz(questions, quizContainer, feedbackContainer, submitButton)
                 submitButton.disabled = true;
             }; 
         };           
-        feedbackContainer.innerHTML = feedback;  
+        feedbackContainer.innerHTML = feedback; 
     };
 
 
